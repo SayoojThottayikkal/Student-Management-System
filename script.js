9f6c5fc858821ca065dc828a32de885a45d6acb5
SayoojThottayikkal <sayoojt99@gmail.com>
2024-12-03 19:39:00 +0530

    js  updated

--- script.js
+++ script.js
@@ -10,30 +10,3 @@ function login() {
   }
   console.log(login);
 }
-
-function added() {
-  const adding = document.getElementsByClassName("field").value;
-  const input = document.getElementById("input");
-
-  // const name = document.getElementById("name").value;
-  // const classe = document.getElementById("classes");
-  // const grade = document.getElementById("grade").value;
-  // const phone = document.getElementById("phone").value;
-  let data = [(Name = ""), (classes = ""), (grade = ""), (phone = "")];
-
-  input.innerHTML = data.map((value) => {
-    return `
-      
-              <tr>
-                <td class="Name">${Name.value}</td>
-                <td class="classses">${classes.value}</td>
-                <td class="Grade">${grade.value}</td>
-                <td class="Action">${phone.value}</td>
-                <td><button id="del_btn">Delete</button></td>
-              </tr>
-           
-          
-    `;
-  });
-  console.log(data);
-}
